1c20f7a45e015201137599c6dab70532ff809dae
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-01 21:37:56 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,9 +4,8 @@ require_relative './fancy_dance.rb'
 
 class Kid
   
-  extend MetaDancing
-  
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
   attr_accessor :name
   
